d9e54968665d50a742f50d888ea4c5b61ca6a045
Aniekan <ani8dar@gmail.com>
2025-01-02 21:26:02 -0400

    DevTrack: Initial commit with existing project files

--- backend/internals/models/user.go
+++ backend/internals/models/user.go
@@ -5,10 +5,12 @@ import (
 )
 
 type User struct {
-	ID             primitive.ObjectID `bson:"_id"`             // User id
-	Teams          []TeamInfo         `bson:"teams"`           // List of team ids and roles in the teams
-	Username       string             `bson:"username"`        // Unique username of the user
+	ID             primitive.ObjectID `bson:"_id"`      // User id
+	Teams          []TeamInfo         `bson:"teams"`    // List of team ids and roles in the teams
+	Username       string             `bson:"username"` // username of the user
+	Email          string             `bson:"email"`
 	HashedPassword []byte             `bson:"hashed_password"` // Hashed password of the user
+	IsVerified     bool               `bson"isVerified`
 }
 
 type TeamInfo struct {
